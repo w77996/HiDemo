7f7b0667994781e940fd65eff56af692683ed049
wuhaihui <912513404@qq.com>
2021-04-06 18:01:49 +0800

    删除key

--- hi-mqtt/src/main/java/com/w77996/mqtt/ali/MQ4IoTSendMessageToMQ4IoTUseSignatureMode.java
+++ hi-mqtt/src/main/java/com/w77996/mqtt/ali/MQ4IoTSendMessageToMQ4IoTUseSignatureMode.java
@@ -25,19 +25,19 @@ public class MQ4IoTSendMessageToMQ4IoTUseSignatureMode {
         /**
          * MQ4IOT 实例 ID，购买后控制台获取
          */
-        String instanceId = "post-cn-n6w1yywbs1m";
+        String instanceId = "";
         /**
          * 接入点地址，购买 MQ4IOT 实例，且配置完成后即可获取，接入点地址必须填写分配的域名，不得使用 IP 地址直接连接，否则可能会导致客户端异常。
          */
-        String endPoint = "post-cn-n6w1yywbs1m.mqtt.aliyuncs.com";
+        String endPoint = "";
         /**
          * 账号 accesskey，从账号系统控制台获取
          */
-        String accessKey = "LTAI4G48ZCtorxEEiuvGFmst";
+        String accessKey = "";
         /**
          * 账号 secretKey，从账号系统控制台获取，仅在Signature鉴权模式下需要设置
          */
-        String secretKey = "EagavgFsTLdkjiGIjs2DOguHnZX1ca";
+        String secretKey = "";
         /**
          * MQ4IOT clientId，由业务系统分配，需要保证每个 tcp 连接都不一样，保证全局唯一，如果不同的客户端对象（tcp 连接）使用了相同的 clientId 会导致连接异常断开。
          * clientId 由两部分组成，格式为 GroupID@@@DeviceId，其中 groupId 在 MQ4IOT 控制台申请，DeviceId 由业务方自己设置，clientId 总长度不得超过64个字符。
@@ -47,7 +47,7 @@ public class MQ4IoTSendMessageToMQ4IoTUseSignatureMode {
          * MQ4IOT 消息的一级 topic，需要在控制台申请才能使用。
          * 如果使用了没有申请或者没有被授权的 topic 会导致鉴权失败，服务端会断开客户端连接。
          */
-        final String parentTopic = "TOPIC_DEV_TML";
+        final String parentTopic = "";
         /**
          * MQ4IOT支持子级 topic，用来做自定义的过滤，此处为示意，可以填写任何字符串，具体参考https://help.aliyun.com/document_detail/42420.html?spm=a2c4g.11186623.6.544.1ea529cfAO5zV3
          * 需要注意的是，完整的 topic 长度不得超过128个字符。
